947e81ceac11f9ef852399a22f0d90dab699c3ba
aiguya <aiguya180@gmail.com>
2019-03-07 17:18:44 +0900

    first merge
    첫 파일 생성

---
+++ PipeShooting/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float speed = 10.0f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    private Transform cameraTr;
+    private Transform originCameraTr;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cameraTr = Camera.main.transform;
+        originCameraTr = cameraTr;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        Move(x, y);
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, v, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+        cameraTr.position = transform.position * 0.5f + new Vector3(0, 0, -15);
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.AngleAxis(x * 90.0f, Vector3.back);
+    }
+}
